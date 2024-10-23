331e32619bfda01d09090b83df99978963e775c8
michal7kw <michal.k7w@gmail.com>
2024-10-23 15:29:13 +0200

    sync

---
+++ Visualization/Genome_Browser_Format.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=convert_narrowPeak_to_BED
+#SBATCH --account=kubacki.michal
+#SBATCH --mem=32GB
+#SBATCH --time=INFINITE
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mail-type=ALL
+#SBATCH --exclusive
+#SBATCH --mail-user=kubacki.michal@hsr.it
+#SBATCH --error="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_ChipSeq/Visualization/logs/genome_browser.err"
+#SBATCH --output="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_ChipSeq/Visualization/logs/genome_browser.out"
+
+# Create output directory if it doesn't exist
+mkdir -p /beegfs/scratch/ric.broccoli/kubacki.michal/SRF_ChipSeq/Visualization/output
+
+# Convert narrowPeak to BED format
+for file in /beegfs/scratch/ric.broccoli/kubacki.michal/SRF_ChipSeq/custom_pipeline/results/peaks/*_peaks.narrowPeak; do
+    filename=$(basename "$file")
+    output_file="/beegfs/scratch/ric.broccoli/kubacki.michal/SRF_ChipSeq/Visualization/output/${filename%.narrowPeak}.bed"
+    cut -f 1-6 "$file" > "$output_file"
+done
